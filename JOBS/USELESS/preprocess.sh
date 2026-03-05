f113293b9df16e2fe6d8624aa34bb780964098f2
Victorien De Meyer <victorien.demeyer@gmail.com>
2026-03-05 11:47:46 -0500

    Stop tracking ignored folder

--- JOBS/USELESS/preprocess.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=40:00:00
-#SBATCH --account=rrg-gachon
-#SBATCH --mem=40G
-#SBATCH --ntasks=1
-
-module load python/3.11 mpi4py/4.0.3 scipy-stack/2024a geos proj; source /home/vdemeyer/py3/bin/activate
-
-python /home/vdemeyer/TRACKING/KATJA/PREPROCESSING/preprocess.py $sim
