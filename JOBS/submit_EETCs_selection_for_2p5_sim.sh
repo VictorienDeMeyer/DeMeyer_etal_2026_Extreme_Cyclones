9e71dca3efaeee921f9971a322600a7c2236d6c8
Victorien De Meyer <victorien.demeyer@gmail.com>
2026-03-06 18:06:27 -0500

    fix: restore full simulation list in submit_EETCs_selection_for_2p5_sim.sh

--- JOBS/submit_EETCs_selection_for_2p5_sim.sh
+++ JOBS/submit_EETCs_selection_for_2p5_sim.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# sims=("ERA5" "UBB" "UBD" "UBE" "UBF")
-sims=("UBI")
+sims=("ERA5" "UBB" "UBD" "UBE" "UBF" "UBG" "UBH" "UBI")
 
 for sim in "${sims[@]}"; do
     sbatch --export=sim=$sim --job-name=${sim}_EETCs_selection_for_2p5_sim -o "/home/vdemeyer/TRACKING/KATJA/JOBS/OUTPUTS/${sim}_EETCs_selection_for_2p5_sim.out" /home/vdemeyer/TRACKING/KATJA/JOBS/EETCs_selection_for_2p5_sim.sh
