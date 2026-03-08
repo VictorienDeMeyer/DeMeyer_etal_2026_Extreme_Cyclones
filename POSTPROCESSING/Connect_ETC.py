70d58a09464679c21015eaa4327326a9e8046852
Victorien De Meyer <victorien.demeyer@gmail.com>
2026-03-08 15:08:11 -0400

    fix: restore full list of simulations and update historical data loading logic

--- POSTPROCESSING/Connect_ETC.py
+++ POSTPROCESSING/Connect_ETC.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 #run ~/TRACKING/KATJA/POSTPROCESSING/Connect_ETC.py
 
-# simulations = ['ERA5', 'UBB', 'UBD', 'UBE', 'UBF', 'UBG', 'UBH', 'UBI']
-simulations = ['UBG', 'UBH', 'UBI']
+simulations = ['ERA5', 'UBB', 'UBD', 'UBE', 'UBF', 'UBG', 'UBH', 'UBI']
 hist_future_map = {'UBG': 'UBD', 'UBH': 'UBE', 'UBI': 'UBF'}
 
 for sim in simulations:
@@ -29,19 +28,19 @@ for sim in simulations:
     if sim in hist_future_map:
         hist_sim = hist_future_map[sim]
         #When historical sim will be run year by year
-        # for year in range(1979, 2015):
-        #     df = pd.read_csv(
-        #     f'/home/vdemeyer/projects/rrg-gachon/vdemeyer/TRACKING/KATJA/OUTPUTS/{hist_sim}_psl_smoothed_400km_12h_1005hPa_{year}_1month.txt',
-        #     sep=r'\s+', header=14, engine='python',
-        #     names=['storm','point','i','j','date','lat','lon','pressure']
-        #     )
-        hist_periods = ['1979-1979', '1980-1989', '1990-1999', '2000-2009', '2010-2014']
-        for period in hist_periods:
+        for year in range(1979, 2015):
             df = pd.read_csv(
-            f'/home/vdemeyer/projects/rrg-gachon/vdemeyer/TRACKING/KATJA/OUTPUTS/{hist_sim}_psl_smoothed_400km_12h_1005hPa_{period}_1month.txt',
+            f'/home/vdemeyer/projects/rrg-gachon/vdemeyer/TRACKING/KATJA/OUTPUTS/{hist_sim}_psl_smoothed_400km_12h_1005hPa_{year}_1month.txt',
             sep=r'\s+', header=14, engine='python',
             names=['storm','point','i','j','date','lat','lon','pressure']
             )
+        # hist_periods = ['1979-1979', '1980-1989', '1990-1999', '2000-2009', '2010-2014']
+        # for period in hist_periods:
+        #     df = pd.read_csv(
+        #     f'/home/vdemeyer/projects/rrg-gachon/vdemeyer/TRACKING/KATJA/OUTPUTS/{hist_sim}_psl_smoothed_400km_12h_1005hPa_{period}_1month.txt',
+        #     sep=r'\s+', header=14, engine='python',
+        #     names=['storm','point','i','j','date','lat','lon','pressure']
+        #     )
             df['date'] = pd.to_datetime(df['date'].astype(str), format='%Y%m%d%H', errors='coerce')
             dfs.append(df)
 
